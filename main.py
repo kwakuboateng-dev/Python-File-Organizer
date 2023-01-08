e7768772d79aa5038a53e1365350dfd31e5c9cf0
Kwaku Boateng <kwakuboateng.dev@outlook.com>
2023-01-08 15:03:08 +0000

    organize function created

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ class FileOrganizer:
     def __init__(self, file_path):
         self.file_path = file_path
         self.files = os.listdir(file_path)
+
+    def organize(self):
+        for file in self.files:
+            filename, extension = os.path.splitext(file)
+            extension = extension[1:]
+            if os.path.exists(f'{self.file_path}/{extension}'):
+                shutil.move(f'{self.file_path}/{file}',
+                            f'{self.file_path}/{extension}/{file}')
+            else:
+                os.makedirs(f'{self.file_path}/{extension}')
+                shutil.move(f'{self.file_path}/{file}',
+                            f'{self.file_path}/{extension}/{file}')
